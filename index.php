f2ae9e15786829a226df8d4812de4f5ef06f69fb
Antoine <antoine.vaxelaire@viacesi.fr>
2020-02-27 14:17:28 +0100

    Ajout template

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+include("setup.php");
+?>
+
+<!DOCTYPE html>
+
+<html lang="fr">
+<?php include("head.php"); ?>
+
+<body>
+<?php include("nav.php"); ?>
+<?php include("hero.php"); ?>
+
+
+<?php include("footer.php"); ?>
+
+</body>
+<?php include("js.php"); ?>
+
+</html>
