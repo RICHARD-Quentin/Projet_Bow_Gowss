c9ef19f7919968f7a2345c935be9c7f84a87d84d
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2020-02-27 20:04:09 +0100

    Gestion de l'ajout de recette

--- class/recipes.php
+++ class/recipes.php
@@ -1,7 +1,41 @@
 <?php
 
-
+include_once ('connexion.php');
 class recipes
 {
 
+    public function register($title, $content, $duree, $image, $persons, $isVegan, $user_id)
+    {
+        $bdd = connexion::connexionBdd();
+        $reg = $bdd->prepare('INSERT INTO recipe (title, content, image, duree, persons, isVegan, user_id) VALUES (:title, :content, :image, :duree, :persons, :isVegan, :user_id)');
+        $reg->execute(array(
+            'title'=>$title,
+            'content'=>$content,
+            'image'=>$image,
+            'duree'=>$duree,
+            'persons'=>$persons,
+            'isVegan'=>$isVegan,
+            'user_id'=>$user_id
+        ));
+        $reg->debugDumpParams();
+        #header('index.php');
+    }
+
+    public function update($id, $title, $content, $duree, $image)
+    {
+        $bdd = connexion::connexionBdd();
+        $updt = $bdd->prepare('UPDATE recipe 
+                        SET title= :title,
+                        content= :content,
+                        image= :image, 
+                        duree= :duree  
+                        WHERE id=:id');
+
+        $updt->execute(array($title,$content,$image,$duree,$id));
+    }
+    public function delete($id){
+        $bdd = connexion::connexionBdd();
+        $del = $bdd->prepare('DELETE FROM recipe WHERE id=:id');
+        $del-> execute(array($id));
+    }
 }
--- recipe.php
+++ recipe.php
@@ -1,1 +1,53 @@
 <?php
+include("setup.php");
+?>
+
+<!DOCTYPE html>
+
+<html lang="fr">
+<?php include("head.php"); ?>
+
+<body>
+<?php include("nav.php"); ?>
+<?php include("hero.php"); ?>
+<main>
+    <div class="mx-auto block border-gray-500">
+        <form action= "traitment/recipe.php" method="post">
+            <div class="mb-2">
+                <label>Titre</label>
+                <input type="text" name="title" class="border rounded border-gray-500">
+            </div>
+            <div class="mb-2">
+                <label>Image</label>
+                <input type="file" name="image" class="border rounded border-gray-500">
+            </div>
+            <div  class="mb-2">
+                <label>Durée</label>
+                <input type="text" name="duree" class="border rounded border-gray-500">
+            </div>
+            <div  class="mb-2">
+                <label>Vegan</label>
+                <input type="checkbox" name="isVegan" class="border rounded border-gray-500 w-12">
+            </div>
+            <div  class="mb-2">
+                <label>Nombre de personnes</label>
+                <input type="number" name="persons" class="border rounded border-gray-500 w-12 text-center">
+            </div>
+            <div  class="mb-2">
+                <label>Contenu</label>
+                <textarea name="content" class="border rounded border-gray-500"></textarea>
+            </div>
+        <input type="submit" value="Envoyer">
+        </form>
+    </div>
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="js/script.js"></script>
+
+<?php include("footer.php"); ?>
+
+</body>
+<?php include("js.php"); ?>
+
+</html>
