da9afbdb83db593a18bd8d2ad3ad22b7c78568c1
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2020-02-28 13:43:31 +0100

    Modif recette

--- recipe.php
+++ recipe.php
@@ -1,5 +1,9 @@
 <?php
 include("setup.php");
+include_once ('class/connexion.php');
+$bdd = connexion::connexionBdd();
+
+
 ?>
 
 <!DOCTYPE html>
@@ -11,35 +15,64 @@ include("setup.php");
 <?php include("nav.php"); ?>
 <?php include("hero.php"); ?>
 <main>
-    <div class="mx-auto block border-gray-500">
-        <form action= "traitment/recipe.php" method="post">
-            <div class="mb-2">
-                <label>Titre</label>
-                <input type="text" name="title" class="border rounded border-gray-500">
+    <form action= "traitment/traitementRecipe.php" method="post">
+        <div class="mx-auto border-gray-500 flex-row flex-wrap mx-auto">
+
+            <div class="w-1/3 inline-block">
+                <div>
+                    <label class="w-1/3">Titre</label>
+                    <input type="text" name="title" class="w-1/2 border rounded border-gray-500">
+                </div>
+                <div class="mb-2">
+                    <label class="w-1/3">Image</label>
+                    <input type="file" name="image" class="w-1/2 border rounded border-gray-500">
+                </div>
+                <div  class="mb-2">
+                    <label class="w-1/3">Durée</label>
+                    <input type="text" name="duree" class="w-1/2 border rounded border-gray-500">
+                </div>
+                <div  class="mb-2">
+                    <label class="w-1/3">Vegan</label>
+                    <input type="checkbox" name="isVegan" class="w-1/2 border rounded border-gray-500 w-12">
+                </div>
+                <div  class="mb-2">
+                    <label class="w-1/3">Nombre de personnes</label>
+                    <input type="number" name="persons" class="w-1/2 border rounded border-gray-500 w-12 text-center">
+                </div>
+                <div  class="mb-2">
+                    <label class="w-1/3">Contenu</label>
+                    <textarea name="content" class="w-1/2 border rounded border-gray-500"></textarea>
+                </div>
             </div>
-            <div class="mb-2">
-                <label>Image</label>
-                <input type="file" name="image" class="border rounded border-gray-500">
+            <div class="w-1/3 inline-block text-center">
+                <label>Ingredients</label>
+                <?php
+                $type = $bdd ->query("SELECT * FROM typeingredient");
+                $typ=$type->fetchAll(PDO::FETCH_CLASS);
+
+                $stmt = $bdd -> prepare("SELECT * FROM ingredient WHERE typeId=:typ");
+
+                foreach ($typ as $tp){
+                    $typeIngredient=$tp->type;
+                    $idIngredient=$tp->id;
+                    echo '<h2>' .  $typeIngredient . '</h2>';
+
+                    $stmt->execute(array("typ"=>$idIngredient));
+                    $nom=$stmt->fetchAll(PDO::FETCH_CLASS);
+
+                    foreach ($nom as $name){
+
+                        echo '<div class="checkbox"><input type="checkbox" v-model="checked" value="' . $name->name . '"><label>' . $name->name . '</label>
+                                    <div v-show="checked"><label>Quantité en g</label><input type="text" class="border border-gray-500"></div></div>';
+                    }
+                }
+                ?>
             </div>
-            <div  class="mb-2">
-                <label>Durée</label>
-                <input type="text" name="duree" class="border rounded border-gray-500">
-            </div>
-            <div  class="mb-2">
-                <label>Vegan</label>
-                <input type="checkbox" name="isVegan" class="border rounded border-gray-500 w-12">
-            </div>
-            <div  class="mb-2">
-                <label>Nombre de personnes</label>
-                <input type="number" name="persons" class="border rounded border-gray-500 w-12 text-center">
-            </div>
-            <div  class="mb-2">
-                <label>Contenu</label>
-                <textarea name="content" class="border rounded border-gray-500"></textarea>
-            </div>
-        <input type="submit" value="Envoyer">
-        </form>
-    </div>
+        </div>
+        <input type="submit" value="Envoyer" class="inline-block mx-auto">
+
+    </form>
+
 </main>
 
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
