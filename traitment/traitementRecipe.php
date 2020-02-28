63d205c3378def5fd22ef89b4488cb28c1b6af2c
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2020-02-28 16:50:53 +0100

    gestion des images des recettes

--- traitment/traitementRecipe.php
+++ traitment/traitementRecipe.php
@@ -6,11 +6,22 @@ $classe = new recipes;
 $title=$_POST['title'];
 $duree=$_POST['duree'];
 $image=1; /*$_POST['image'];*/
-$isVegan=$_POST['isVegan'];
+
+if (isset($_POST['isVegan'])){
+    $isVegan=1;
+}
+else {
+    $isVegan=0;
+}
+
+$path=__DIR__;
+$recipeImage=$_FILES['image'];
+$recipeImagePath= recipes::image($path,$recipeImage);
+
 $persons=$_POST['persons'];
 $content=$_POST['content'];
 #$id=$_SESSION['id'];
 $user_id=1;
 
 
-$classe->register($title, $content, $duree , $image, $persons, $isVegan, $user_id);
+$classe->register($title, $content, $duree , $recipeImagePath, $persons, $isVegan, $user_id);
