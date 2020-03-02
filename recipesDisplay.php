e8014705e40dfb9f3cda81598b6594088f6185f5
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2020-03-02 16:26:54 +0100

    design du formulaire et de la recette

--- recipesDisplay.php
+++ recipesDisplay.php
@@ -29,37 +29,39 @@ foreach ($list as $lst) {
     $stmt->execute(array('id'=>$lst->id));
     $step=$stmt->fetchAll(PDO::FETCH_CLASS);
     ?>
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img class="w-full" src="<?php echo $lst->image ?>" alt="Sunset in the mountains">
+    <div class="max-w-5xl w-5/6 rounded overflow-hidden shadow-lg mx-auto my-6">
+        <img class="w-full h-64" src="<?php echo $lst->image ?>" alt="Sunset in the mountains">
         <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2"><?php echo $lst->title ?></div>
-            <div>
-            <p class="text-gray-700 text-base w-2/5">
+            <div class="mx-auto flex flex-col">
+            <div class="font-bold text-xl mb-2 mx-auto inline-block"><?php echo $lst->title ?></div>
+            <div class="px-6 py-4 mx-auto inline-block">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Nombre de personnes :<?php echo $lst->persons ?></span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Preparation :<?php echo $lst->duree ?>h</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Cuisson :<?php echo $lst->cuisson ?>h</span>
+            </div>
+            </div>
+            <div class="flex">
+            <ul class="text-gray-700 text-base w-1/5">
 
             <?php
 
                 foreach ($ing as $ingr){
-                    echo $ingr->ingredient . ' : ' . $ingr->quantity . '<br>';
+                    echo '<li>' . $ingr->ingredient . ' : ' . $ingr->quantity . '<br>';
 
                 } ?>
-            </p>
-                <ul class="w-2/5">
+            </ul>
+                <ul class="w-4/5">
                     <?php
                     $i=1;
                     foreach ($step as $stp) {
-                        echo '<li>Etape ' . $i . ' : ' . $stp->steps . '</li>';
+                        echo '<li class="mb-2">Etape ' . $i . ' : ' . $stp->steps . '</li>';
                         $i++;
                     }
                     ?>
                 </ul>
             </div>
         </div>
-        <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Durée :<?php echo $lst->duree ?>h</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Nombre de personnes :<?php echo $lst->cuisson ?>h</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Nombre de personnes :<?php echo $lst->persons ?></span>
 
-        </div>
     </div>
 <?php } ?>
 <?php include("template/footer.php"); ?>
