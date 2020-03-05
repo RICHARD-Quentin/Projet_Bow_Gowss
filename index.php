7d397987030bb75098c798088f391f9914238124
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2020-03-05 16:08:05 +0100

    amelioration favoris// responsive

--- index.php
+++ index.php
@@ -27,14 +27,28 @@ $bdd = connexion::connexionBdd();
 <?php
 $stmt=$bdd->query("SELECT recipe.id, title, content, image, duree, cuisson, persons, isVegan, user_id, nickname, isAdmin FROM recipe INNER JOIN user ON recipe.user_id=user.id");
 $list=$stmt->fetchAll(PDO::FETCH_CLASS);
-
     foreach ($list as $lst)
     {
+        $id=$lst->id;
+        //Test favorite
+        $fav= $bdd->prepare("SELECT * FROM favorite WHERE user=:user && recipe=:recipe");
+        $fav->execute(array("recipe"=>$id,
+            "user"=>$_SESSION['id_session']
+        ));
+        $isFavorite=$fav->fetch(PDO::FETCH_BOUND);
+        if($isFavorite){
+            $isFavoriteClass="true text-red-600 hover:text-gray-400";
+        }
+        else{
+            $isFavoriteClass="false text-gray-400 hover:text-red-600";
+        }
 ?>
-            <div class="mx-4 inline-block w-1/3 max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl ">
+            <div class="mx-4 inline-block md:w-1/3 mb-2 max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl ">
                 <a href="recipeTemplate.php?id=<?php echo $lst->id ?>">
                     <img class="w-full h-64" src="<?php echo $lst->image ?>" alt="Sunset in the mountains">
-                    <div class="px-6 py-4">
+                    <div class="px-6 py-4 relative">
+                        <i id="fav" class="<?php echo $isFavoriteClass ?> fa-lg fas fa-heart absolute right-0 m-2"><input class="recipe" type="hidden" value="<?php echo $id ?>"><input class="user" type="hidden" value="<?php echo $_SESSION['id_session'] ?>"></i>
+
                         <div class="font-bold text-xl mb-2"><?php echo $lst->title ?></div>
                         <p>Par <?php echo $lst->nickname ?></p>
                         <p class="text-gray-700 text-base">
